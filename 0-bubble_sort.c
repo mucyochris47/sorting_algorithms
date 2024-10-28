6921e64dd1b71abc7014abab36a91bc9f3e53759
mucyochris47 <mucyochris250@gmail.com>
2024-10-28 03:24:57 -0700

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,27 +1,33 @@
 #include "sort.h"
-#include <stdio.h>
+/**
+ *bubble_sort - Function that sorts an array of ints
+ *using bubble sort algorithm
+ *
+ *@array: array of ints
+ *@size: size of array
+ *
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i, k;
 
-void bubble_sort(int *array, size_t size) {
-    size_t i, j;
-    int temp;
-    int swapped;
+	if (!array || !size)
+		return;
 
-    if (array == NULL || size < 2)
-        return;
-
-    for (i = 0; i < size - 1; i++) {
-        swapped = 0;
-        for (j = 0; j < size - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                print_array(array, size);
-                swapped = 1;
-            }
-        }
-        if (!swapped)
-            break;
-    }
+	i = 0;
+	while (i < size)
+	{
+		for (k = 0; k < size - 1; k++)
+		{
+			if (array[k] > array[k + 1])
+			{
+				temp = array[k];
+				array[k] = array[k + 1];
+				array[k + 1] = temp;
+				print_array(array, size);
+			}
+		}
+		i++;
+	}
 }
-
