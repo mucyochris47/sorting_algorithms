6fc68e94a3d6b00cb42005183c6b8db927aec0f9
mucyochris47 <mucyochris250@gmail.com>
2024-11-02 13:39:41 -0700

    task 4

--- sort.h
+++ sort.h
@@ -2,11 +2,10 @@
 #define SORT_H
 
 #include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  * struct listint_s - Doubly linked list node
+ *
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
@@ -18,10 +17,16 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+/* Function prototypes */
 void print_array(const int *array, size_t size);
-void selection_sort(int *array, size_t size);
 void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+/* Add other prototypes as necessary */
 
 #endif /* SORT_H */
 
